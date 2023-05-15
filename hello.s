97d71f02f0e648a313a43c270a4e593500bc6708
Yilin Yang <yyl_20050115@hotmail.com>
2023-05-16 02:50:00 +0800

    arm32

--- hello.s
+++ hello.s
@@ -1,36 +1,20 @@
-/* Data segment: define our message string and calculate its length. */
-
-msg:
-
-    .ascii        "Hello, ARM64! Written by Yiiln.\n"
-
-len = . - msg
-
+.syntax unified
+.data
+message:
+        .ascii "Hello, world.\n"
+len = . - message
 .text
-
-/* Our application's entry point. */
-
-.globl _start
-
+.global _start
 _start:
+        /* syscall write(int fd, const void *buf, size_t count) */
+        mov     %r0, $1     /* fd -> stdout */
+        ldr     %r1, =message   /* buf -> message */
+        ldr     %r2, =len   /* count -> len(message) */
+        mov     %r7, $4     /* write is syscall #4 */
+        swi     $0          /* invoke syscall */
 
-    /* syscall write(int fd, const void *buf, size_t count) */
-
-	mov     r0, r0 
-	mov     r1, r1
-	add     r0, r0,#0
-	add     r0, r0,#1
-	cmp     r0, #0
-	cmp     r0, #1
-	cmp     r0, r1
-	mov     r0, #1      /* fd := STDOUT_FILENO */
-	ldr     r1, =msg    /* buf := msg */
-	ldr     r2, =len    /* count := len */
-	mov     r8, #64     /* write is syscall #64 */
-	svc     #0          /* invoke syscall */
-
-    /* syscall exit(int status) */
-	mov     r0, #0      /* status := 0 */
-	mov     r8, #93     /* exit is syscall #93 */
-	svc     #0          /* invoke syscall */
+        /* syscall exit(int status) */
+        mov     %r0, $0     /* status -> 0 */
+        mov     %r7, $1     /* exit is syscall #1 */
+        swi     $0          /* invoke syscall */
 
