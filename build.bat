68efde7b03b10cecd13be0a9c1057abff8111397
yilin <yyl_20050115@hotmail.com>
2023-05-16 02:14:20 +0800

    update

---
+++ build.bat
@@ -0,0 +1,3 @@
+as -o hello.o hello.s
+ld -s -o hello hello.o
+
---
+++ run.bat
@@ -0,0 +1,8 @@
+adb push hello /data/local/tmp
+adb shell "chmod 777 /data/local/tmp/hello"
+adb shell "ls -la /data/local/tmp/hello"
+adb shell "/data/local/tmp/hello"
+
+
+#Output:
+#Hello, ARM64! Written by Yilin
